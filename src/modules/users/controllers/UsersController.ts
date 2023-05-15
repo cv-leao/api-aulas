3f4d958fcfd219939ffb545297c489bf2b487a50
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-05-14 21:08:47 -0300

    Criação do controller de usuários e adição do service CreateUserService no controller

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import CreateUserService from "../services/CreateUserService";
+
+export default class UsersController {
+    public async create(request: Request, response: Response) {
+        const { name, email, level , password, samePasswords } =
+            request.body;
+
+        const createUser = new CreateUserService();
+
+        const user = await createUser
+            .execute({
+                name,
+                email,
+                level,
+                password,
+                samePasswords,
+            })
+            .catch(error => {
+                response.statusCode = 400;
+                return error;
+            });
+/*
+        if (user.id) {
+            const session = new SessionsController();
+            session.create(request, response);
+        } else {
+            return response.json(user);
+        }
+*/
+        return response.json(user);
+    }
+
+}
