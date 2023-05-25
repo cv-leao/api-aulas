390ada47e669980079767cd30092f85ab3434892
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-05-25 16:42:17 -0300

    Criando a rota de login

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import usersRouter from "../../modules/users/routes/users.routes";
-import emailRouter from "../../modules/users/routes/email.routes";
+import usersRouter from "../../../modules/users/routes/users.routes";
+import emailRouter from "../../../modules/users/routes/email.routes";
+import sessionsRouter from "../../../modules/users/routes/sessions.routes";
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/user/session", sessionsRouter);
 
 routes.use("/email", emailRouter);
 
