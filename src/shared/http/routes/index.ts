4bb6355bed7e64dc8c32d2707aea79e2ee442edd
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-05-28 02:52:56 -0300

    Adicionando do middleware de autenticação na rota classroom

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import usersRouter from "../../../modules/users/routes/users.routes";
 import emailRouter from "../../../modules/users/routes/email.routes";
 import sessionsRouter from "../../../modules/users/routes/sessions.routes";
 import classroomsRouter from "../../../modules/classrooms/routes/classrooms.routes";
+import isAuthenticated from "../middlewares/isAuthenticated";
 
 const routes = Router();
 
@@ -13,6 +14,6 @@ routes.use("/user/session", sessionsRouter);
 routes.use("/email", emailRouter);
 
 //Classrooms
-routes.use("/classroom", classroomsRouter);
+routes.use("/classroom", isAuthenticated, classroomsRouter);
 
 export default routes;
