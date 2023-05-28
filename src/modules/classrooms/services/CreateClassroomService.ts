594aadfc6a29d57ba2f3b8a2eb582843e7c3a861
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-05-28 02:28:15 -0300

    Criação do service de criar sala de aula

---
+++ src/modules/classrooms/services/CreateClassroomService.ts
@@ -0,0 +1,51 @@
+import { Classroom } from "@prisma/client";
+import { prisma } from "../../../database/prismaClient";
+import AppError from "../../../shared/errors/AppError";
+import generateClassroomCode from "../../../utils/GenerateClassroomCode";
+
+interface IClassroomToCreate {
+    user_id: string;
+    name: string;
+}
+
+class CreateClassroomService {
+    public async execute({user_id, name}: IClassroomToCreate): Promise<Classroom> {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: user_id
+            },
+        });
+
+        if(!user) {
+            throw new AppError("Usuário não encontrado.");
+        }
+
+        if(user.active === false) {
+            throw new AppError("Usuário não encontrado.");
+        }
+
+        const code = generateClassroomCode();
+
+        if(!code) {
+            throw new AppError("Erro ao gerar código da sala.");
+        }
+
+        const classroom = await prisma.classroom.create({
+            data: {
+                name: name,
+                code: code,
+                active_room: true,
+                administrators: {
+                    connect: { id: user_id },
+                }
+            },
+            include: {
+                administrators: true,
+            },
+        });
+
+        return classroom;
+    }
+}
+
+export default CreateClassroomService;
