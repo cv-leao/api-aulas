79884b5ad70160ffbeb6d69487fa2847f14eede9
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-05-30 22:08:32 -0300

    Criando a rota de promoteAParticipantToAdmin

--- src/modules/classrooms/routes/classrooms.routes.ts
+++ src/modules/classrooms/routes/classrooms.routes.ts
@@ -6,5 +6,6 @@ const classroomsController = new ClassroomsController
 
 classroomsRouter.post("/create", classroomsController.create);
 classroomsRouter.patch("/join/:code", classroomsController.addParticipantWithCode);
+classroomsRouter.patch("/promote_to_administrator", classroomsController.promoteAParticipantToAdmin);
 
 export default classroomsRouter;
