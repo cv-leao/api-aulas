7bf77803cbe99bd1d71284f4abf18e27666f3cd3
Carlos Victor Leão Almondes <carlosvictorleao@gmail.com>
2023-06-07 13:52:16 -0300

    Criando a rota que retorna todas as salas de aula de um usuário

--- src/modules/classrooms/routes/classrooms.routes.ts
+++ src/modules/classrooms/routes/classrooms.routes.ts
@@ -7,5 +7,6 @@ const classroomsController = new ClassroomsController
 classroomsRouter.post("/create", classroomsController.create);
 classroomsRouter.patch("/join/:code", classroomsController.addParticipantWithCode);
 classroomsRouter.patch("/promote_to_administrator", classroomsController.promoteAParticipantToAdmin);
+classroomsRouter.get("/show_user_classrooms", classroomsController.showUserClassrooms);
 
 export default classroomsRouter;
